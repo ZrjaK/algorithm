d4173409de0957ff836d8053cdd33e6f4a92f127
ZrjaK <2629680369@qq.com>
2022-09-28 09:25:49 +0800

    修改并查集模板

--- 模板/python/UnionFind.py
+++ 模板/python/UnionFind.py
@@ -1,15 +1,17 @@
-n = 10**5
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.part = n
 
-parent = list(range(n))
-cnt = [1] * n
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
 
-def find(i):
-    if parent[i] != i:
-        parent[i] = find(parent[i])
-    return parent[i]
-
-def union(i, j):
-    x, y = find(i), find(j)
-    if x != y:
-        cnt[y] += cnt[x]
-        parent[x] = parent[y]
+    def union(self, i, j):
+        x, y = self.find(i), self.find(j)
+        if x != y:
+            self.size[y] += self.size[x]
+            self.parent[x] = self.parent[y]
+            self.part -= 1
