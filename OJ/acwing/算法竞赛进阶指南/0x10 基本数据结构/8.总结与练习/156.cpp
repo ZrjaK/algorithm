382f5b1b9aae65dd02443b3168c72b569a247676
ZrjaK <2629680369@qq.com>
2022-11-16 16:23:53 +0800

    更新题目

---
+++ OJ/acwing/算法竞赛进阶指南/0x10 基本数据结构/8.总结与练习/156.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+// #define ll __int128
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pdd pair<ld, ld>
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define vll vector<ll>
+#define vvll vector<vector<ll> >
+#define lb lower_bound
+#define ub upper_bound
+#define pb push_back
+#define pf push_front
+#define ppf pop_front
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define rep(i, a, b) for(ll i = a; i < b; ++i)
+#define per(i, a, b) for(ll i = a; i > b; --i)
+#define each(x, v) for(auto& x: v)
+#define len(x) x.size()
+#define elif else if
+#define all(x) begin(x), end(x)
+#define mst(x, a) memset(x, a, sizeof(x))
+#ifndef lowbit
+#define lowbit(x) (x & (-x))
+#endif
+#define bitcnt(x) (__builtin_popcountll(x))
+#define _up(x) (int)ceil(1.0*x)
+#define _down(x) (int)floor(1.0*x)
+#define endl "\n"
+template <class T>
+using pq = priority_queue<T>;
+template <class T>
+using pqg = priority_queue<T, vector<T>, greater<T> >;
+const __int128 ONE = 1;
+ll gcd(ll x,ll y) {
+	if(!x) return y;
+	if(!y) return x;
+	int t = __builtin_ctzll(x | y);
+	x >>= __builtin_ctzll(x);
+	do {
+		y >>= __builtin_ctzll(y);
+		if(x > y) swap(x, y);
+		y -= x;
+	} while(y);
+	return x<<t;
+}
+ll lcm(ll x, ll y) { return x * y / gcd(x, y); }
+ll max(ll x, ll y) { return x > y ? x : y; }
+ll min(ll x, ll y) { return x < y ? x : y; }
+ll Mod(ll x, int mod) { return (x % mod + mod) % mod; }
+ll pow(ll x, ll y, ll mod){
+	ll res = 1, cur = x;
+	while (y) {
+		if (y & 1)	res = res * cur % mod;
+		cur = ONE * cur * cur % mod;
+		y >>= 1;
+	}
+	return res % mod;
+}
+ll probabilityMod(ll x, ll y, ll mod) {
+	return x * pow(y, mod-2, mod) % mod;
+}
+const ll LINF = 0x1fffffffffffffff;
+const ll MINF = 0x7fffffffffff;
+const int INF = 0x3fffffff;
+const int MOD = 1000000007;
+const int MODD = 998244353;
+const int N = 1e3 + 10;
+
+ull p[N];
+ull h[N][N];
+string s;
+unordered_set<ull> v;
+ll m, n, a, b, q;
+
+void solve() {
+	cin >> m >> n >> a >> b;
+	p[0] = 1;
+	rep(i, 1, N) p[i] = p[i-1] * 131;
+	rep(i, 1, m+1) {
+		cin >> s;
+		rep(j, 1, n+1) {
+			h[i][j] = h[i][j-1] * 131 + s[j-1] - '0';
+		}
+	}
+	auto calc = [&] (int x, int y) {
+		ull t = 0;
+		rep(i, x-a+1, x+1) t = t * p[b] + h[i][y] - h[i][y-b] * p[b];
+		return t;
+	};
+	rep(i, a, m+1) rep(j, b, n+1) v.insert(calc(i, j));
+	cin >> q;
+	rep(_, 0, q) {
+		ull t = 0;
+		rep(i, 0, a) {
+			cin >> s;
+			rep(j, 0, b) t = t * 131 + s[j] - '0';
+		}
+		cout << int(v.find(t) != v.end()) << endl;
+	}
+	
+}
+
+signed main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+    int t = 1;
+	// cin >> t;
+    while (t--) {
+        solve();
+    }
+	return 0;
+}
