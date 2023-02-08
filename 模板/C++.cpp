8fbd5c8a06924708f628cf0fa7341acff9dc0958
ZrjaK <2629680369@qq.com>
2023-02-08 23:40:17 +0800

    更新题目

--- 模板/C++.cpp
+++ 模板/C++.cpp
@@ -63,9 +63,9 @@ struct custom_hash {
     }
 
     size_t operator()(pair<uint64_t,uint64_t> x) const {
-		static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-		return splitmix64(x.first + FIXED_RANDOM) ^ (splitmix64(x.second + FIXED_RANDOM) >> 1);
-	}
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x.first + FIXED_RANDOM) ^ (splitmix64(x.second + FIXED_RANDOM) >> 1);
+    }
 };
 const i128 ONE = 1;
 istream &operator>>(istream &in, i128 &x) {
