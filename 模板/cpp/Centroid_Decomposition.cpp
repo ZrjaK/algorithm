fd3cb8baa2c3ae8df58ca16e0b5c1e28ff9caba5
ZrjaK <2629680369@qq.com>
2023-11-02 09:46:32 +0800

    更新模板

--- 模板/cpp/Centroid_Decomposition.cpp
+++ 模板/cpp/Centroid_Decomposition.cpp
@@ -1,205 +1,370 @@
+#line 2 "graph/base.hpp"
+
+template <typename T>
+struct Edge {
+  int frm, to;
+  T cost;
+  int id;
+};
+
 template <typename T = int, bool directed = false>
 struct Graph {
-    int n;
-    using cost_type = T;
-    using edge_type = tuple<int, int, cost_type, int>;
-    vector<vector<pair<int, cost_type>>> g;
+  static constexpr bool is_directed = directed;
+  int N, M;
+  using cost_type = T;
+  using edge_type = Edge<T>;
+  vector<edge_type> edges;
+  vector<int> indptr;
+  vector<edge_type> csr_edges;
+  vc<int> vc_deg, vc_indeg, vc_outdeg;
+  bool prepared;
 
-    vector<edge_type> edges;
-    constexpr bool is_directed() { return directed; }
+  class OutgoingEdges {
+  public:
+    OutgoingEdges(const Graph* G, int l, int r) : G(G), l(l), r(r) {}
 
-    Graph(int _n) : n(_n), g(_n) {}
-
-    void add(int u, int v, cost_type cost, int i = -1) {
-        assert(0 <= u && u < n && 0 <= v && v < n);
-        edges.emplace_back(u, v, cost, i);
+    const edge_type* begin() const {
+      if (l == r) { return 0; }
+      return &G->csr_edges[l];
     }
 
-    void read_tree(bool wt = false, int off = 1) { read_graph(n - 1, wt, off); }
+    const edge_type* end() const {
+      if (l == r) { return 0; }
+      return &G->csr_edges[r];
+    }
 
-    void read_graph(int m, bool wt = false, int off = 1) {
-        for (int i = 0; i < m; i++) {
-            int a, b; cin >> a >> b;
-            a -= off, b -= off;
-            cost_type cost = 1;
-            if (wt) cin >> cost;
-            add(a, b, cost, i);
+  private:
+    const Graph* G;
+    int l, r;
+  };
+
+  bool is_prepared() { return prepared; }
+
+  Graph() : N(0), M(0), prepared(0) {}
+  Graph(int N) : N(N), M(0), prepared(0) {}
+
+  void build(int n) {
+    N = n, M = 0;
+    prepared = 0;
+    edges.clear();
+    indptr.clear();
+    csr_edges.clear();
+    vc_deg.clear();
+    vc_indeg.clear();
+    vc_outdeg.clear();
+  }
+
+  void add(int frm, int to, T cost = 1, int i = -1) {
+    assert(!prepared);
+    assert(0 <= frm && 0 <= to && to < N);
+    if (i == -1) i = M;
+    auto e = edge_type({frm, to, cost, i});
+    edges.eb(e);
+    ++M;
+  }
+
+  // wt, off
+  void read_tree(bool wt = false, int off = 1) { read_graph(N - 1, wt, off); }
+
+  void read_graph(int M, bool wt = false, int off = 1) {
+    for (int m = 0; m < M; ++m) {
+      INT(a, b);
+      a -= off, b -= off;
+      if (!wt) {
+        add(a, b);
+      } else {
+        T c;
+        read(c);
+        add(a, b, c);
+      }
+    }
+    build();
+  }
+
+  void build() {
+    assert(!prepared);
+    prepared = true;
+    indptr.assign(N + 1, 0);
+    for (auto&& e: edges) {
+      indptr[e.frm + 1]++;
+      if (!directed) indptr[e.to + 1]++;
+    }
+    for (int v = 0; v < N; ++v) { indptr[v + 1] += indptr[v]; }
+    auto counter = indptr;
+    csr_edges.resize(indptr.back() + 1);
+    for (auto&& e: edges) {
+      csr_edges[counter[e.frm]++] = e;
+      if (!directed)
+        csr_edges[counter[e.to]++] = edge_type({e.to, e.frm, e.cost, e.id});
+    }
+  }
+
+  OutgoingEdges operator[](int v) const {
+    assert(prepared);
+    return {this, indptr[v], indptr[v + 1]};
+  }
+
+  vc<int> deg_array() {
+    if (vc_deg.empty()) calc_deg();
+    return vc_deg;
+  }
+
+  pair<vc<int>, vc<int>> deg_array_inout() {
+    if (vc_indeg.empty()) calc_deg_inout();
+    return {vc_indeg, vc_outdeg};
+  }
+
+  int deg(int v) {
+    if (vc_deg.empty()) calc_deg();
+    return vc_deg[v];
+  }
+
+  int in_deg(int v) {
+    if (vc_indeg.empty()) calc_deg_inout();
+    return vc_indeg[v];
+  }
+
+  int out_deg(int v) {
+    if (vc_outdeg.empty()) calc_deg_inout();
+    return vc_outdeg[v];
+  }
+
+  void debug() {
+    print("Graph");
+    if (!prepared) {
+      print("frm to cost id");
+      for (auto&& e: edges) print(e.frm, e.to, e.cost, e.id);
+    } else {
+      print("indptr", indptr);
+      print("frm to cost id");
+      FOR(v, N) for (auto&& e: (*this)[v]) print(e.frm, e.to, e.cost, e.id);
+    }
+  }
+
+  vc<int> new_idx;
+  vc<bool> used_e;
+
+  // G における頂点 V[i] が、新しいグラフで i になるようにする
+  // {G, es}
+  pair<Graph<T, directed>, vc<int>> rearrange(vc<int> V) {
+    if (len(new_idx) != N) new_idx.assign(N, -1);
+    if (len(used_e) != M) used_e.assign(M, 0);
+    int n = len(V);
+    FOR(i, n) new_idx[V[i]] = i;
+    Graph<T, directed> G(n);
+    vc<int> es;
+    FOR(i, n) {
+      for (auto&& e: (*this)[V[i]]) {
+        if (used_e[e.id]) continue;
+        int a = e.frm, b = e.to;
+        if (new_idx[a] != -1 && new_idx[b] != -1) {
+          used_e[e.id] = 1;
+          G.add(new_idx[a], new_idx[b], e.cost);
+          es.eb(e.id);
         }
-        build();
+      }
     }
-    
-    void build() {
-        for (auto&& [u, v, cost, i] : edges) {
-            g[u].emplace_back(v, cost);
-            if (!directed) g[v].emplace_back(u, cost);
-        }
-    }
+    FOR(i, n) new_idx[V[i]] = -1;
+    for (auto&& eid: es) used_e[eid] = 0;
+    G.build();
+    return {G, es};
+  }
 
-    vector<pair<int, cost_type>> operator[] (int i) {
-        return g[i];
-    }
+private:
+  void calc_deg() {
+    assert(vc_deg.empty());
+    vc_deg.resize(N);
+    for (auto&& e: edges) vc_deg[e.frm]++, vc_deg[e.to]++;
+  }
+
+  void calc_deg_inout() {
+    assert(vc_indeg.empty());
+    vc_indeg.resize(N);
+    vc_outdeg.resize(N);
+    for (auto&& e: edges) { vc_indeg[e.to]++, vc_outdeg[e.frm]++; }
+  }
 };
+#line 2 "graph/centroid.hpp"
+
+// (v,w) or (v,-1)
 
 template <typename GT>
 pair<int, int> find_centroids(GT& G) {
-    int n = G.n;
-    vector<int> parent(n, -1);
-    vector<int> sz(n);
-    vector<int> q = {0};
-    for (int i = 0; i < q.size(); i++) {
-        int u = q[i];
-        for (auto&& [v, w]: G[u])
-        if (v != parent[u]) {
-            parent[v] = u;
-            q.emplace_back(v);
-        }
-    }
-    for (int i = n - 1; i >= 0; i--) {
-        int v = q[i];
-        sz[v] += 1;
-        int p = parent[v];
-        if (p != -1) sz[p] += sz[v];
-    }
+  static_assert(!GT::is_directed);
+  int N = G.N;
+  vc<int> par(N, -1);
+  vc<int> V(N);
+  vc<int> sz(N);
+  int l = 0, r = 0;
+  V[r++] = 0;
+  while (l < r) {
+    int v = V[l++];
+    for (auto&& e: G[v])
+      if (e.to != par[v]) {
+        par[e.to] = v;
+        V[r++] = e.to;
+      }
+  }
+  FOR_R(i, N) {
+    int v = V[i];
+    sz[v] += 1;
+    int p = par[v];
+    if (p != -1) sz[p] += sz[v];
+  }
 
-    int m = n / 2;
-    auto check = [&](int u) -> bool {
-        if (n - sz[u] > m) return false;
-        for (auto&& [v, w]: G[u]) {
-            if (v != parent[u] && sz[v] > m) return false;
-        }
-        return true;
-    };
-    pair<int, int> ans = {-1, -1};
-    for (int i = 0; i < n; i++) if (check(i)) {
-        if (ans.first != -1) {
-            ans.second = i;
-        } else {
-            ans.first = i;
-        }
+  int M = N / 2;
+  auto check = [&](int v) -> bool {
+    if (N - sz[v] > M) return false;
+    for (auto&& e: G[v]) {
+      if (e.to != par[v] && sz[e.to] > M) return false;
     }
-    return ans;
+    return true;
+  };
+  pair<int, int> ANS = {-1, -1};
+  FOR(v, N) if (check(v)) {
+    if (ANS.fi != -1) {
+      ANS.se = v;
+    } else {
+      ANS.fi = v;
+    }
+  }
+  return ANS;
 }
 
 template <typename GT>
 struct Centroid_Decomposition {
-    using edge_type = typename GT::edge_type;
-    GT& G;
-    int n;
-    vector<int> sz;
-    vector<int> parent;
-    vector<int> cdep; // depth in centroid tree
+  using edge_type = typename GT::edge_type;
+  GT& G;
+  int N;
+  vc<int> sz;
+  vc<int> par;
+  vector<int> cdep; // depth in centroid tree
 
-    vector<int> dparent;
+  bool calculated;
 
-    bool calculated;
-
-    Centroid_Decomposition(GT& G)
-        : G(G), n(G.n), sz(G.n), parent(G.n), cdep(G.n, -1), dparent(n, -1) {
-        calculated = 0;
-        build();
-    }
+  Centroid_Decomposition(GT& G)
+      : G(G), N(G.N), sz(G.N), par(G.N), cdep(G.N, -1) {
+    calculated = 0;
+    build();
+  }
 
 private:
-  int find(int u) {
-    vector<int> q = {u};
-    parent[u] = -1;
+  int find(int v) {
+    vc<int> V = {v};
+    par[v] = -1;
     int p = 0;
-    while (p < q.size()) {
-      int u = q[p++];
-      sz[u] = 0;
-      for (auto&& [v, w] : G[u]) {
-        if (v == parent[u] || cdep[v] != -1) continue;
-        parent[v] = u;
-        q.emplace_back(v);
+    while (p < len(V)) {
+      int v = V[p++];
+      sz[v] = 0;
+      for (auto&& e: G[v]) {
+        if (e.to == par[v] || cdep[e.to] != -1) continue;
+        par[e.to] = v;
+        V.eb(e.to);
       }
     }
-    while (q.size()) {
-        int u = q.back();
-        q.pop_back();
-        sz[u] += 1;
-        if (p - sz[u] <= p / 2) return u;
-        sz[parent[u]] += sz[u];
+    while (len(V)) {
+      int v = V.back();
+      V.pop_back();
+      sz[v] += 1;
+      if (p - sz[v] <= p / 2) return v;
+      sz[par[v]] += sz[v];
     }
     return -1;
   }
   void build() {
+    assert(G.is_prepared());
     assert(!calculated);
     calculated = 1;
 
-    vector<tuple<int, int, int>> st;
-    st.emplace_back(0, 0, -1);
+    vc<pair<int, int>> st;
+    st.eb(0, 0);
     while (!st.empty()) {
-        auto [lv, v, fa] = st.back();
-        st.pop_back();
-        auto c = find(v);
-        dparent[c] = fa;
-        cdep[c] = lv;
-        for (auto&& [to, w] : G[c]) {
-            if (cdep[to] == -1) { st.emplace_back(lv + 1, to, c); }
-        }
+      auto [lv, v] = st.back();
+      st.pop_back();
+      auto c = find(v);
+      cdep[c] = lv;
+      for (auto&& e: G[c]) {
+        if (cdep[e.to] == -1) { st.eb(lv + 1, e.to); }
+      }
     }
   }
 
 public:
-  // vector of pairs (v, path data v)
+  // V, dat, indptr
 
-  template <typename E, typename F>
-  vector<vector<pair<int, E>>> collect(int root, E root_val, F f) {
-    vector<vector<pair<int, E>>> res = {{{root, root_val}}};
-    for (auto&& [nxt, w] : G[root]) {
+  template <typename T, typename F>
+  tuple<vc<int>, vc<T>, vc<int>> collect_path_data(int root, T root_val, F f) {
+    vc<int> V = {root};
+    vc<T> dp = {root_val};
+    vc<int> indptr = {0, 1};
+    for (auto&& e: G[root]) {
+      int nxt = e.to;
       if (cdep[nxt] < cdep[root]) continue;
-      vector<pair<int, E>> dat;
-      int p = 0;
-      dat.eb(nxt, f(root_val, tuple<int, int, E>{root, nxt, w}));
-      parent[nxt] = root;
-      while (p < len(dat)) {
-        auto [u, val] = dat[p++];
-        for (auto&& [v, w] : G[u]) {
-          if (v == parent[u]) continue;
-          if (cdep[v] < cdep[root]) continue;
-          parent[v] = u;
-          dat.emplace_back(v, f(val, tuple<int, int, E>{u, v, w}));
+      int p = len(V);
+      V.eb(nxt);
+      dp.eb(f(root_val, e));
+      par[nxt] = root;
+      while (p < len(V)) {
+        int v = V[p];
+        T val = dp[p];
+        p++;
+        for (auto&& e: G[v]) {
+          if (e.to == par[v]) continue;
+          if (cdep[e.to] < cdep[root]) continue;
+          par[e.to] = v;
+          V.eb(e.to);
+          dp.eb(f(val, e));
         }
       }
-      res.emplace_back(dat);
-      res[0].insert(res[0].end(), dat.begin(), dat.end());
+      indptr.eb(len(V));
     }
-    return res;
+    return {V, dp, indptr};
   }
 
-  vector<vector<pair<int, int>>> collect_dist(int root) {
+  // V, dist, indptr
+
+  tuple<vc<int>, vc<int>, vc<int>> collect_dist(int root) {
     auto f = [&](int x, auto e) -> int { return x + 1; };
-    return collect(root, 0, f);
+    return collect_path_data(root, 0, f);
   }
 
-  // (V, H), (V[i] in G) = (i in H).
+  // (V, H, indptr), (V[i] in G) = (i in H).
 
   // 0,1,2... is a dfs order in H.
 
-  pair<vector<int>, Graph<typename GT::cost_type, true>> get_subgraph(int root) {
-    static vector<int> conv;
-    while (len(conv) < n) conv.emplace_back(-1);
+  tuple<vc<int>, Graph<typename GT::cost_type, true>, vc<int>> get_subgraph(
+      int root) {
+    static vc<int> conv;
+    while (len(conv) < N) conv.eb(-1);
 
-    vector<int> q;
+    vc<int> V = {root};
+    vc<int> indptr = {0, 1};
+    conv[root] = 0;
     using cost_type = typename GT::cost_type;
-    vector<tuple<int, int, cost_type>> edges;
+    vc<tuple<int, int, cost_type>> edges;
 
-    auto dfs = [&](auto& dfs, int u, int p) -> void {
-      conv[u] = len(q);
-      q.emplace_back(u);
-      for (auto&& [v, cost] : G[u]) {
-        if (v == p) continue;
-        if (cdep[v] < cdep[root]) continue;
-        dfs(dfs, v, u);
-        edges.emplace_back(conv[u], conv[v], cost);
+    auto dfs = [&](auto& dfs, int v, int p) -> void {
+      conv[v] = len(V);
+      V.eb(v);
+      for (auto&& e: G[v]) {
+        int to = e.to;
+        if (to == p) continue;
+        if (cdep[to] < cdep[root]) continue;
+        dfs(dfs, to, v);
+        edges.eb(conv[v], conv[to], e.cost);
       }
     };
-    dfs(dfs, root, -1);
-    int n = len(q);
+    for (auto&& e: G[root]) {
+      if (cdep[e.to] < cdep[root]) continue;
+      dfs(dfs, e.to, root);
+      edges.eb(conv[root], conv[e.to], e.cost);
+      indptr.eb(len(V));
+    }
+    int n = len(V);
     Graph<typename GT::cost_type, true> H(n);
     for (auto&& [a, b, c]: edges) H.add(a, b, c);
     H.build();
-    for (auto&& v: q) conv[v] = -1;
-    return {q, H};
+    for (auto&& v: V) conv[v] = -1;
+    return {V, H, indptr};
   }
 };
