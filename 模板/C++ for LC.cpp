34a93d53f8daa1f5487ee32bf6802264e9d4f230
ZrjaK <2629680369@qq.com>
2023-11-08 14:59:02 +0800

    更新模板

--- 模板/C++ for LC.cpp
+++ 模板/C++ for LC.cpp
@@ -21,7 +21,6 @@ using u32  =                unsigned int;
 using u64  =                unsigned long long;
 using i128 =                __int128;
 using u128 =                __uint128_t;
-using f128 =                __float128;
 using ld   =                long double;
 using ui   =                unsigned int;
 using ull  =                unsigned long long;
@@ -253,277 +252,98 @@ struct custom_hash {
         return splitmix64(x.first + FIXED_RANDOM) ^ (splitmix64(x.second + FIXED_RANDOM) >> 1);
     }
 };
-#define FASTIO
-#include <unistd.h>
-
-// https://judge.yosupo.jp/submission/21623
-
-namespace fastio {
-static constexpr uint32_t SZ = 1 << 17;
-char ibuf[SZ];
-char obuf[SZ];
-char out[100];
-// pointer of ibuf, obuf
-
-uint32_t pil = 0, pir = 0, por = 0;
-
-struct Pre {
-  char num[10000][4];
-  constexpr Pre() : num() {
-    for (int i = 0; i < 10000; i++) {
-      int n = i;
-      for (int j = 3; j >= 0; j--) {
-        num[i][j] = n % 10 | '0';
-        n /= 10;
-      }
+const i128 ONE = 1;
+// istream &operator>>(istream &in, i128 &x) {
+//     string s;
+//     in >> s;
+//     bool minus = false;
+//     if (s[0] == '-') {
+//         minus = true;
+//         s.erase(s.begin());
+//     }
+//     x = 0;
+//     for (auto i : s) {
+//         x *= 10;
+//         x += i - '0';
+//     }
+//     if (minus) x = -x;
+//     return in;
+// }
+ostream &operator<<(ostream &out, i128 x) {
+    string s;
+    bool minus = false;
+    if (x < 0) {
+        minus = true;
+        x = -x;
     }
-  }
-} constexpr pre;
-
-inline void load() {
-  memcpy(ibuf, ibuf + pil, pir - pil);
-  pir = pir - pil + fread(ibuf + pir - pil, 1, SZ - pir + pil, stdin);
-  pil = 0;
+    while (x) {
+        s.push_back(x % 10 + '0');
+        x /= 10;
+    }
+    if (s.empty()) s = "0";
+    if (minus) out << '-';
+    reverse(s.begin(), s.end());
+    out << s;
+    return out;
 }
-
-inline void flush() {
-  fwrite(obuf, 1, por, stdout);
-  por = 0;
+template <class T> ostream &operator<<(ostream &os, const set<T> &v) {
+    for(auto it = begin(v); it != end(v); ++it) {
+        if(it == begin(v)) os << *it;
+        else os << " " << *it;
+    }
+    return os;
 }
-
-void rd(char &c) {
-  do {
-    if (pil + 1 > pir) load();
-    c = ibuf[pil++];
-  } while (isspace(c));
+template <class T> ostream &operator<<(ostream &os, const multiset<T> &v) {
+    for(auto it = begin(v); it != end(v); ++it) {
+        if(it == begin(v)) os << *it;
+        else os << " " << *it;
+    }
+    return os;
 }
-
-void rd(string &x) {
-  x.clear();
-  char c;
-  do {
-    if (pil + 1 > pir) load();
-    c = ibuf[pil++];
-  } while (isspace(c));
-  do {
-    x += c;
-    if (pil == pir) load();
-    if (pil == pir) break;
-    c = ibuf[pil++];
-  } while (!isspace(c));
+template <class T> ostream &operator<<(ostream &os, const pbds_set<T> &v) {
+    for(auto it = begin(v); it != end(v); ++it) {
+        if(it == begin(v)) os << *it;
+        else os << " " << *it;
+    }
+    return os;
 }
-
-template <typename T>
-void rd_real(T &x) {
-  string s;
-  rd(s);
-  x = stod(s);
+template <class T, class S> istream &operator>>(istream &in, pair<T, S> &p) {
+    in >> p.first >> p.second;
+    return in;
 }
-
-template <typename T>
-void rd_integer(T &x) {
-  if (pil + 100 > pir) load();
-  char c;
-  do
-    c = ibuf[pil++];
-  while (c < '-');
-  bool minus = 0;
-  if constexpr (is_signed<T>::value || is_same_v<T, i128>) {
-    if (c == '-') { minus = 1, c = ibuf[pil++]; }
-  }
-  x = 0;
-  while (c >= '0') { x = x * 10 + (c & 15), c = ibuf[pil++]; }
-  if constexpr (is_signed<T>::value || is_same_v<T, i128>) {
-    if (minus) x = -x;
-  }
+template <class T, class S> ostream &operator<<(ostream &os, const pair<T, S> &p) {
+    os << p.first << " " << p.second;
+    return os;
 }
-
-void rd(int &x) { rd_integer(x); }
-void rd(ll &x) { rd_integer(x); }
-// void rd(i128 &x) { rd_integer(x); }
-void rd(u32 &x) { rd_integer(x); }
-void rd(u64 &x) { rd_integer(x); }
-void rd(u128 &x) { rd_integer(x); }
-void rd(double &x) { rd_real(x); }
-void rd(long double &x) { rd_real(x); }
-void rd(f128 &x) { rd_real(x); }
-
-template <class T, class U>
-void rd(pair<T, U> &p) {
-  return rd(p.first), rd(p.second);
+template <class T, size_t size> istream &operator>>(istream &in, array<T, size> &v) {
+    for(auto& x : v) in >> x;
+    return in;
 }
-template <size_t N = 0, typename T>
-void rd_tuple(T &t) {
-  if constexpr (N < std::tuple_size<T>::value) {
-    auto &x = std::get<N>(t);
-    rd(x);
-    rd_tuple<N + 1>(t);
-  }
+template <class T, size_t size> ostream &operator<<(ostream &os, const array<T, size> &v) {
+    for(int i = 0; i < size; i++) {
+        if(i == 0) os << v[i];
+        else os << " " << v[i];
+    }
+    return os;
 }
-template <class... T>
-void rd(tuple<T...> &tpl) {
-  rd_tuple(tpl);
+template <class T> istream &operator>>(istream &in, vector<T> &v) {
+    for(auto& x : v) in >> x;
+    return in;
 }
-
-template <size_t N = 0, typename T>
-void rd(array<T, N> &x) {
-  for (auto &d: x) rd(d);
+template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
+    for(auto it = begin(v); it != end(v); ++it) {
+        if(it == begin(v)) os << *it;
+        else os << " " << *it;
+    }
+    return os;
 }
-template <class T>
-void rd(vc<T> &x) {
-  for (auto &d: x) rd(d);
+inline void print() { std::cout << '\n'; }
+template <typename Head, typename... Tail>
+inline void print(const Head& head, const Tail &...tails) {
+    std::cout << head;
+    if (sizeof...(tails)) std::cout << ' ';
+    print(tails...);
 }
-
-void read() {}
-template <class H, class... T>
-void read(H &h, T &... t) {
-  rd(h), read(t...);
-}
-
-void wt(const char c) {
-  if (por == SZ) flush();
-  obuf[por++] = c;
-}
-void wt(const string s) {
-  for (char c: s) wt(c);
-}
-void wt(const char *s) {
-  size_t len = strlen(s);
-  for (size_t i = 0; i < len; i++) wt(s[i]);
-}
-
-template <typename T>
-void wt_integer(T x) {
-  if (por > SZ - 100) flush();
-  if (x < 0) { obuf[por++] = '-', x = -x; }
-  int outi;
-  for (outi = 96; x >= 10000; outi -= 4) {
-    memcpy(out + outi, pre.num[x % 10000], 4);
-    x /= 10000;
-  }
-  if (x >= 1000) {
-    memcpy(obuf + por, pre.num[x], 4);
-    por += 4;
-  } else if (x >= 100) {
-    memcpy(obuf + por, pre.num[x] + 1, 3);
-    por += 3;
-  } else if (x >= 10) {
-    int q = (x * 103) >> 10;
-    obuf[por] = q | '0';
-    obuf[por + 1] = (x - q * 10) | '0';
-    por += 2;
-  } else
-    obuf[por++] = x | '0';
-  memcpy(obuf + por, out + outi + 4, 96 - outi);
-  por += 96 - outi;
-}
-
-template <typename T>
-void wt_real(T x) {
-  ostringstream oss;
-  oss << fixed << setprecision(15) << double(x);
-  string s = oss.str();
-  wt(s);
-}
-
-void wt(int x) { wt_integer(x); }
-void wt(ll x) { wt_integer(x); }
-// void wt(i128 x) { wt_integer(x); }
-void wt(u32 x) { wt_integer(x); }
-void wt(u64 x) { wt_integer(x); }
-void wt(u128 x) { wt_integer(x); }
-void wt(double x) { wt_real(x); }
-void wt(long double x) { wt_real(x); }
-void wt(f128 x) { wt_real(x); }
-
-template <class T, class U>
-void wt(const pair<T, U> val) {
-  wt(val.first);
-  wt(' ');
-  wt(val.second);
-}
-template <size_t N = 0, typename T>
-void wt_tuple(const T t) {
-  if constexpr (N < std::tuple_size<T>::value) {
-    if constexpr (N > 0) { wt(' '); }
-    const auto x = std::get<N>(t);
-    wt(x);
-    wt_tuple<N + 1>(t);
-  }
-}
-template <class... T>
-void wt(tuple<T...> tpl) {
-  wt_tuple(tpl);
-}
-template <class T, size_t S>
-void wt(const array<T, S> val) {
-  auto n = val.size();
-  for (size_t i = 0; i < n; i++) {
-    if (i) wt(' ');
-    wt(val[i]);
-  }
-}
-template <class T>
-void wt(const vector<T> val) {
-  auto n = val.size();
-  for (size_t i = 0; i < n; i++) {
-    if (i) wt(' ');
-    wt(val[i]);
-  }
-}
-
-void print() { wt('\n'); }
-template <class Head, class... Tail>
-void print(Head &&head, Tail &&... tail) {
-  wt(head);
-  if (sizeof...(Tail)) wt(' ');
-  print(forward<Tail>(tail)...);
-}
-
-// gcc expansion. called automaticall after main.
-
-void __attribute__((destructor)) _d() { flush(); }
-} // namespace fastio
-
-using fastio::read;
-using fastio::print;
-using fastio::flush;
-
-#define INT(...)   \
-  int __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define LL(...)   \
-  ll __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define U32(...)   \
-  u32 __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define U64(...)   \
-  u64 __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define STR(...)      \
-  string __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define CHAR(...)   \
-  char __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define DBL(...)      \
-  double __VA_ARGS__; \
-  read(__VA_ARGS__)
-
-#define VEC(type, name, size) \
-  vector<type> name(size);    \
-  read(name)
-#define VV(type, name, h, w)                     \
-  vector<vector<type>> name(h, vector<type>(w)); \
-  read(name)
-
-void YES(bool t = 1) { print(t ? "YES" : "NO"); }
-void NO(bool t = 1) { YES(!t); }
-void Yes(bool t = 1) { print(t ? "Yes" : "No"); }
-void No(bool t = 1) { Yes(!t); }
-void yes(bool t = 1) { print(t ? "yes" : "no"); }
-void no(bool t = 1) { yes(!t); }
 template <typename Iterable>
 auto print_all(const Iterable& v, std::string sep = " ", std::string end = "\n") -> decltype(std::cout << *v.begin(), void()) {
     for (auto it = v.begin(); it != v.end();) {
@@ -532,7 +352,39 @@ auto print_all(const Iterable& v, std::string sep = " ", std::string end = "\n")
     }
     std::cout << end;
 }
-const i128 ONE = 1;
+void read() {}
+template <class Head, class... Tail>
+void read(Head &head, Tail &... tail) {
+    cin >> head;
+    read(tail...);
+}
+#define INT(...)   \
+    int __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define LL(...)   \
+    ll __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define STR(...)      \
+    string __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define CHAR(...)   \
+    char __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define DBL(...)      \
+    double __VA_ARGS__; \
+    read(__VA_ARGS__)
+#define VEC(type, name, size) \
+    vector<type> name(size);    \
+    read(name)
+#define VV(type, name, h, w)                     \
+    vector<vector<type>> name(h, vector<type>(w)); \
+    read(name)
+void YES(bool t = 1) { print(t ? "YES" : "NO"); }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { print(t ? "Yes" : "No"); }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { print(t ? "yes" : "no"); }
+void no(bool t = 1) { yes(!t); }
 ll gcd(ll x, ll y) {
     if(!x) return y;
     if(!y) return x;
