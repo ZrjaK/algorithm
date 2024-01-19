ec2e046187dcb1435325f7611b20119d12528de0
ZrjaK <2629680369@qq.com>
2024-01-19 09:52:44 +0800

    fix

--- OJ/codeforces/1920/F2.cpp
+++ OJ/codeforces/1920/F2.cpp
@@ -614,22 +614,21 @@ vc<int> parallel_binary_search(int Q, int ok, int ng, F1 init, F2 upd,
                                F3 check) {
   int T = max(ok, ng);
   vc<int> OK(Q, ok), NG(Q, ng);
-  vc<vc<int>> C(T);
   int flag = 1;
   while (flag) {
     flag = 0;
     init();
+    vc<vc<int>> C(T);
     FOR(q, Q) if (abs(OK[q] - NG[q]) > 1) {
       C[(OK[q] + NG[q]) / 2].eb(q);
     }
     FOR_R(t, T) {
       flag |= !C[t].empty();
       upd(t);
-      each(j, C[t]) {
-        if (check(j)) OK[j] = t;
-        else NG[j] = t;
+      for (auto&& q : C[t]) {
+        if (check(q)) OK[q] = t;
+        else NG[q] = t;
       }
-      C[t].clear();
     }
   }
   return OK;
